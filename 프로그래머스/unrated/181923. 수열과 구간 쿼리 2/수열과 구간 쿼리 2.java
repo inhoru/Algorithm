d5e56bb76d30776958052acdf8c283c0c9c16958
Inho Choi <inhoru126@gmail.com>
2023-12-27 17:59:48 +0900

    [level unrated] Title: 수열과 구간 쿼리 2, Time: 1.71 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -8,17 +8,19 @@ class Solution {
            int s = queries[i][0];
            int e = queries[i][1];
            int k = queries[i][2];
-            
+           
             int min = Integer.MAX_VALUE;
             
+            
             for(int j = s; j<=e;j++){
-                if(arr[j] > k&&arr[j] <min){
+                if(arr[j] > k&&min>arr[j]){
                     min = arr[j];
                 }
             }
             if(min != Integer.MAX_VALUE){
                 answer[i] = min;
             }
+          
         }
        
         return answer;
