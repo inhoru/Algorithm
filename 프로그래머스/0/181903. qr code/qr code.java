e9e0a4267b8a56c5dfe1209894b905730cfce4eb
Inho Choi <inhoru126@gmail.com>
2024-02-17 15:43:50 +0900

    [level 0] Title: qr code, Time: 0.06 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -1,11 +1,11 @@
 class Solution {
     public String solution(int q, int r, String code) {
-        String answer = "";
+          StringBuilder sb = new StringBuilder();
           for (int i = 0; i < code.length(); i++) {
             if (i % q == r) {
-                answer += code.charAt(i);
+                 sb.append(code.charAt(i));
             }
         }
-        return answer;
+        return sb.toString();
     }
 }
