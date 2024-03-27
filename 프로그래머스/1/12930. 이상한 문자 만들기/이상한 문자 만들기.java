36b4e1de6d2fb14e18b6ebca5d816f9d1edcea7d
Inho Choi <inhoru126@gmail.com>
2024-03-27 19:31:04 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 4.08 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+          String[] arr = s.split("");
+         int index = 0;
+          for (int i = 0; i < s.length(); i++) {
+            if (arr[i].equals(" ")) {
+                index = 0;
+                answer += " ";
+            } else
+            if (index%2==0){
+               answer += arr[i].toUpperCase();
+               index++;
+            } else if (index%2!=0){
+                answer += arr[i].toLowerCase();
+                index++;
+            }
+        }
+        return answer;
+    }
+}
