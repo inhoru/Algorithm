5b5aac99b8a5510d66bf44008747e1d7d74427c9
Inho Choi <inhoru126@gmail.com>
2024-04-03 20:41:59 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -4,7 +4,7 @@ class Solution {
         String[] arr = {"zero","one","two","three","four","five","six","seven","eight","nine"};
          for(int i = 0; i<arr.length;i++){
             if(s.contains(arr[i])){
-                s = s.replace(arr[i],""+i);
+               s = s.contains(arr[i])?s.replace(arr[i],""+i):s;
             }
         }
         return Integer.parseInt(s);
