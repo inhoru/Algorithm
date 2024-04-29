37705132acd487367c3ea68584a8e6e0cf4ed154
Inho Choi <inhoru126@gmail.com>
2024-04-29 16:43:09 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.23 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String num_str) {
+        
+        int answer = 0;
+        String[] arr = num_str.split("");
+            
+        for(String str : arr){
+            answer += Integer.parseInt(str);
+        }
+        
+        return answer;
+    }
+}
