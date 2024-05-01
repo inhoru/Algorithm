ed336d7311f288dad9953d8309eb204adf94acc1
Inho Choi <inhoru126@gmail.com>
2024-05-01 16:43:38 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,23 +1,12 @@
 class Solution {
     public int solution(int[] date1, int[] date2) {
-         if (date1[0] < date2[0]) {
-            return 1;
-        } else if (date1[0] > date2[0]) {
-            return 0;
-        }
-
-        if (date1[1] < date2[1]){
-            return 1;
-        } else if (date1[1] > date2[1]){
-            return 0;
-        }
-
-        if (date1[2] < date2[2]){
-            return 1;
-        } else if (date1[2] > date2[2]){
-            return 0;
+       for (int i = 0; i < 3; i++) {
+            if (date1[i] < date2[i]) {
+                return 1;
+            } else if (date1[i] > date2[i]) {
+                return 0;
+            }
         }
         return 0;
-
     }
 }
