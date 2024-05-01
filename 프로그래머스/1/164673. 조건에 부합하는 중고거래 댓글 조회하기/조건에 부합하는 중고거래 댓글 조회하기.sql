aade1a19e63ad703d73d3ba2544e982e04b8f428
Inho Choi <inhoru126@gmail.com>
2024-05-01 17:12:20 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -0,0 +1,2 @@
+select a.title, a.board_id, b.reply_id, b.writer_id, b.contents, DATE_FORMAT(b.created_date,'%Y-%m-%d') as created_date from used_goods_board a join used_goods_reply b
+using(board_id) where a.created_date like '2022-10%' order by created_date , a.title
