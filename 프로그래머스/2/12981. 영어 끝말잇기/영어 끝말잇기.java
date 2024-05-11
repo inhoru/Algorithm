9490a23758965449f1e91d6a816bf5087735d197
Inho Choi <inhoru126@gmail.com>
2024-05-11 15:48:02 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,49 +1,45 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-         List<String> list = new ArrayList<>();
 
-        int count = 0;
-        int num = 0;
-        int su = 0;
+        List<String> list = new ArrayList<>();
+
+        int count = 0; // 몇번째순서
+        int num = 0;    // 몇번째사람
+
+        int[] answer = {0, 0};
 
         for (int i = 0; i < words.length; i++) {
             num++;
             if (i % n == 0) {
                 count++;
             }
+//            중복글자확인
             if (list.contains(words[i])) {
-                su++;
+                answer[0] = num;
+                answer[1] = count;
                 break;
+            }else{
+                list.add(words[i]);
             }
-            list.add(words[i]);
 
+//            끝말확인
             if (i != 0) {
                 char first = words[i].charAt(0);
                 char last = list.get(i - 1).charAt(list.get(i - 1).length() - 1);
                 if (first != last) {
-                    su++;
+                    answer[0] = num;
+                    answer[1] = count;
                     break;
                 }
-                if (num % n == 0) {
-                    num = 0;
-                }
-
+            }
+//           순서 초가화
+            if (num % n == 0) {
+                num = 0;
             }
 
         }
-            if(su !=0){
-                int[] answer = {num,count};
-               return answer;
-                
-            }else{
-                 int[] answer = {0,0};
-                return answer;
-                    
-               
-            }
+        return answer;
     
     }
 }
