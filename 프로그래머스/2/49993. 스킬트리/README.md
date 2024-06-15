b56ff6bbb6e662d8c55b6dec790982f552a70903
Inho Choi <inhoru126@gmail.com>
2024-06-15 13:27:21 +0900

    [level 2] Title: 스킬트리, Time: 0.12 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/README.md
+++ 프로그래머스/2/49993. 스킬트리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 스킬트리 - 49993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49993) 
 
 ### 성능 요약
 
-메모리: 77.2 MB, 시간: 0.13 ms
+메모리: 78.4 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 25일 16:39:46
+2024년 06월 15일 13:27:18
 
 ### 문제 설명
 
--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,29 +1,42 @@
 import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-     StringBuilder sb = new StringBuilder();
-     String[] arr = new String[skill_trees.length];
-     int answer = 0;
-          int isEmpty =0;
-        
-          for (int i = 0; i < skill_trees.length; i++) {
+
+        StringBuilder sb = new StringBuilder();
+
+        int answer = 0; // 가능한 스킬트리 인덱스
+        int isEmpty = 0; // 관련없는 스킬만 있을떄 + 해줄 인덱스 (ex "ABC" | ["QWE"],["DSF])
+
+        //새로운 유저 스킬트리 담을 배열
+        String[] arr = new String[skill_trees.length];
+        // skill 트리 말고 다른 스킬들 지우는 메서드 ex) "QFABT" = "AB"
+        for (int i = 0; i < skill_trees.length; i++) {
             for (int j = 0; j < skill_trees[i].length(); j++) {
                 if (skill.contains(String.valueOf(skill_trees[i].charAt(j)))) {
                     sb.append(skill_trees[i].charAt(j));
-
-
                 }
             }
             arr[i] = sb.toString();
             sb.setLength(0);
         }
+
+        // 스킬트리와 관련없는 유저스킬리스트확인 메소드
+        for (String str : arr) {
+            if (str.isEmpty()) {
+                isEmpty++;
+            }
+        }
         
-         for (String userList : arr) {
+        // skill 트리 순서 확인 메서드
+        //userList = [ABC, ASBEC,AB]
+        for (String userList : arr) {
             int index = 0;
+            //c = A ,B , C
             for (char c : userList.toCharArray()) {
-                int skillIndex = skill.indexOf(c);//C(0),B(1),D(2)
+                int skillIndex = skill.indexOf(c);//A(0),B(1),C(2)
                 if (index == skillIndex) {
-                    index ++;
+                    index++;
                 } else {
                     index = 0;
                     break;
@@ -34,14 +47,10 @@ class Solution {
                 answer++;
             }
         }
-         for(String str : arr){
-            if(str.isEmpty()){
-                isEmpty++;
-            }
-        }
-        
-        return answer+isEmpty ;
-        
-        
+
+
+        return answer + isEmpty;
+
+
     }
 }
